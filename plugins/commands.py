e38f728d1877094169691d521e0e4e40b340abaa
sravantony2 <71973948+sravantony2@users.noreply.github.com>
2021-01-15 19:18:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,14 +12,18 @@ logger = logging.getLogger(__name__)
 async def start(bot, message):
     """Start command handler"""
     if len(message.command) > 1 and message.command[1] == 'subscribe':
-        await message.reply(INVITE_MSG)
+        text = INVITE_MSG
+        channel_url = "YOUR CHANNEL URL"
+        buttons = [[InlineKeyboardButton('Join channel', url=channel_url)]]
     else:
+        text = START_MSG
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(START_MSG, reply_markup=reply_markup)
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(text, reply_markup=reply_markup)
 
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
