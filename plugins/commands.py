e7c6f6d80f3bd2c5793d3c14a3626805c877a724
sravantony2 <71973948+sravantony2@users.noreply.github.com>
2021-03-24 09:27:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -123,3 +123,97 @@ async def delete(bot, message):
         await msg.edit('File is successfully deleted from database')
     else:
         await msg.edit('File not found in database')
+
+
+## --- Status | Broadcast --- ##
+
+@Client.on_message(filters.private & filters.command('status') & filters.user(ADMINS))
+async def sts(bot, message):
+    total_users = await mongodb.total_users_count()
+    await message.reply_text(text=f"Total Users in DB: {total_users}", quote=True)
+
+async def forwarder(user_id, message):
+    try:
+        await message.forward(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return forwarder(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} - deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} - blocked the bot\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} - user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} - {traceback.format_exc()}\n"
+
+@Client.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS) & filters.reply)
+async def broadcast_(bot, message):
+    all_users = await mongodb.get_all_users()
+    broadcast_msg = message.reply_to_message
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+    out = await message.reply_text(
+        text = f"Broadcast Started! You will be notified with a log file when all the users are notified."
+    )
+    start_time = time.time()
+    total_users = await mongodb.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(
+        total = total_users,
+        current = done,
+        failed = failed,
+        success = success
+    )
+    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            
+            sts, msg = await forwarder(
+                user_id = int(user['id']),
+                message = broadcast_msg
+            )
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            
+            if sts == 400:
+                await mongodb.delete_user(user['id'])
+            
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(
+                    dict(
+                        current = done,
+                        failed = failed,
+                        success = success
+                    )
+                )
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    if failed == 0:
+        await message.reply_text(
+            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            quote=True
+        )
+    else:
+        await message.reply_document(
+            document='broadcast.txt',
+            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            quote=True
+        )
+    
+    await aiofiles.os.remove('broadcast.txt')
